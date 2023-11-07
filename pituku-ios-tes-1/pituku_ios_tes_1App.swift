4521ed580c8a39b554d89db5ad62223ffe24b940
Hubert Darryl Daryanto <hubertdaryanto@gmail.com>
2023-11-07 09:10:23 +0700

    Initial commit

---
+++ pituku-ios-tes-1/pituku_ios_tes_1App.swift
@@ -0,0 +1,17 @@
+//
+//  pituku_ios_tes_1App.swift
+//  pituku-ios-tes-1
+//
+//  Created by Hubert Daryanto on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pituku_ios_tes_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
